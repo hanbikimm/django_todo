9ac23a58129e902b01c6a6ed10486b13f0038b38
김한비 <zacquisha130@gmail.com>
2021-07-27 10:07:32 +0900

    hmm

--- first/todo/forms.py
+++ first/todo/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from todo.models import Todo
-class TodoForm(forms.Form):
+class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo #사용할 모델
         fields = ['title', 'contents'] # QuestionForm에서 사용할 Question 모델 속성
